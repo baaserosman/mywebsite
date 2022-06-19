08188a1198b80536e10d5f21563233477b78c7be
baaserosman <baaser.osman@gmail.com>
2022-06-19 21:05:11 +0200

    data structured

--- src/helpers/data.jsx
+++ src/helpers/data.jsx
@@ -1,9 +1,16 @@
 export const data = [
   {
-    number: 1,
-    name: "Django-Blog-App",
-    image: "/static/img/blogApp(django).png",
-    url: "https://still-sands-45656.herokuapp.com/",
+    number: 16,
+    name: "Web Grafik Design App",
+    image: "/static/img/webgrafikdesignApp.png",
+    url: "https://bascher.netlify.app/",
+  },
+
+  {
+    number: 3,
+    name: "Bootstrap-Project",
+    image: "/static/img/bootstrapApp.png",
+    url: "https://bootstrapproject-2.netlify.app",
   },
 
   {
@@ -12,84 +19,91 @@ export const data = [
     image: "/static/img/blogApp(react).png",
     url: "https://baschers-blog-app.netlify.app/",
   },
-  {
-    number: 16,
-    name: "Web Grafik Design App",
-    image: "/static/img/webgrafikdesignApp.png",
-    url: "https://bascher.netlify.app/",
-  },
-  {
-    number: 3,
-    name: "Bootstrap-Project",
-    image: "/static/img/bootstrapApp.png",
-    url: "https://bootstrapproject-2.netlify.app",
-  },
+
   {
     number: 4,
     name: "Calculator",
     image: "/static/img/calculator.png",
     url: "https://baaser-ios-calculator.netlify.app/",
   },
+
+  {
+    number: 1,
+    name: "Django-Blog-App",
+    image: "/static/img/blogApp(django).png",
+    url: "https://still-sands-45656.herokuapp.com/",
+  },
+
   {
     number: 5,
     name: "Expence Tracker",
     image: "/static/img/expenceTrackerApp.png",
     url: "https://expencee-tracker.netlify.app",
   },
+
   {
     number: 6,
     name: "Flag-App",
     image: "/static/img/flagApp.png",
     url: "https://js-flag-app.netlify.app",
   },
+
   {
     number: 7,
     name: "Free-Meal-App",
     image: "/static/img/freemealApp.png",
     url: "https://free-meal-api-project.netlify.app",
   },
+
   {
     number: 8,
     name: "Google Landing Page",
     image: "/static/img/googlelandingApp.png",
     url: "https://google-landinggg.netlify.app",
   },
+
   {
     number: 9,
     name: "Movie App",
     image: "/static/img/movieApp.png",
     url: "https://baschers-movie-app.netlify.app",
   },
+
   {
     number: 10,
     name: "Movie Seat Booking App",
     image: "/static/img/movieseatbookingApp.png",
     url: "https://baschers-movieseatbookingapp.netlify.app",
   },
+
   {
     number: 11,
     name: "Netflix Survey Form",
     image: "/static/img/netflixsurveyform.png",
     url: "https://bascher-survey-form.netlify.app",
   },
+
   {
     number: 12,
     name: "Recipe App",
     image: "/static/img/recipeApp.png",
     url: "https://rcp-app.netlify.app",
   },
+
   {
     number: 13,
     name: "Todo App",
     image: "/static/img/todoApp(django).png",
     url: "https://thawing-shore-30799.herokuapp.com/",
   },
+
   {
     number: 14,
     name: "Tour Places App",
     image: "/static/img/tourplacesApp.png",
     url: "https://baschers-tour-places.netlify.app/",
   },
+
   {
     number: 15,
     name: "Weather App",
